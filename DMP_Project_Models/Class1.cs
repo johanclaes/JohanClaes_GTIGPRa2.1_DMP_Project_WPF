7c1c4a1636ca2a6991984e36d762992283960d06
fb922 <fb922@JOHANCLAES>
2022-11-18 21:09:35 +0100

    Add project files.

---
+++ DMP_Project_Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMP_Project_Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ JohanClaes_GTIGPRa2.1_DMP_Project_WPF/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace JohanClaes_GTIGPRa2._1_DMP_Project_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
