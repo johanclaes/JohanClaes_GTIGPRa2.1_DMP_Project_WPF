9fb95b42e5ee637229d83c9a81f8702a9fdac2b9
johanclaes <s0085147@student.thomasmore.be>
2022-11-20 18:54:39 +0100

    met username - password

---
+++ DMP_Project_DAL/IDatabaseConnectie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMP_Project_DAL
+{
+    public interface IDatabaseConnectie
+    {
+        void Connecteren();
+        IDbConnection Connectie ();
+
+        void Open();
+
+        void Close();
+    }
+}
